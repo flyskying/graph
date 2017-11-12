682bea341667e38d4e5dcb3a6d6689d05ba52d55
Amy_oyzl <32362887+Amyoyzl@users.noreply.github.com>
2017-11-12 11:19:58 +0800

    add Cirlce

---
+++ src/edu/cusut/lw/graph/App.java
@@ -0,0 +1,10 @@
+package edu.cusut.lw.graph;
+
+/**
+ * 
+ * @author Amy_oyzl
+ *
+ */
+public class App {
+
+}
--- src/edu/cusut/lw/graph/Point.java
+++ src/edu/cusut/lw/graph/Point.java
@@ -2,9 +2,37 @@ package edu.cusut.lw.graph;
 
 /**
  * 
- * @author lwiii
+ * @author Amy_oyzl
  *
  */
 public class Point {
+private int x;
+	
+	private int y;
+	
+	Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
 
+	public int getY() {
+		return y;
+	}
+
+	void show() {
+		System.out.println("I'm Point!");
+		System.out.println("P:(" + x + "," + y + ")");
+		
+		
+//		for(int j = 0; j < y; j++)
+//			System.out.println();
+//		for(int i = 0; i < x; i++)
+//			System.out.print(" ");
+//		System.out.print("*");
+		
+	}
 }
